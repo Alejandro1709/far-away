015f3ba60bc4737a166d28061c106a589f8e70db
= <alejandro.lr.developer@hotmail.com>
2024-05-23 10:00:10 -0500

    Add: render dummy items and add item component

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,5 +1,20 @@
-function PackingList() {
-  return <div className="list">LIST</div>;
+import Item from "./Item";
+import PropTypes from "prop-types";
+
+function PackingList({ initialItems }) {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PackingList;
+
+PackingList.propTypes = {
+  initialItems: PropTypes.arrayOf(PropTypes.object),
+};
