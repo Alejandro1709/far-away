6834e60221a40c319437a75b7af471031c221ec4
= <alejandro.lr.developer@hotmail.com>
2024-05-23 11:01:07 -0500

    Fix: lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,14 @@ import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
-import { initialItems } from "./data/items";
 
 function App() {
-  const [items, setItems] = useState(initialItems);
-  const [description, setDescription] = useState("");
-  const [qty, setQty] = useState(1);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = {
-      id: items.length + 1,
-      description,
-      qty,
-      packed: false,
-    };
-
-    setItems([...items, newItem]);
-  };
+  const [items, setItems] = useState([]);
 
   return (
     <div className="app">
       <Logo />
-      <Form
-        description={description}
-        qty={qty}
-        setQty={setQty}
-        setDescription={setDescription}
-        onSubmit={handleSubmit}
-      />
+      <Form items={items} setItems={setItems} />
       <PackingList initialItems={items} />
       <Stats initialItems={items} />
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,25 @@
-function Form({ description, qty, setQty, setDescription, onSubmit }) {
+import { useState } from "react";
+
+function Form({ items, setItems }) {
+  const [description, setDescription] = useState("");
+  const [qty, setQty] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: items.length + 1,
+      description,
+      qty,
+      packed: false,
+    };
+
+    setItems([...items, newItem]);
+  };
   return (
-    <form className="add-form" onSubmit={onSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       <select value={qty} onChange={(e) => setQty(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
