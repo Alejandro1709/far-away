8d66781cb53359a4e7a95d9a42ad901dbe4a2da5
= <alejandro.lr.developer@hotmail.com>
2024-05-23 09:48:03 -0500

    Add: Main UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   );
 }
 
